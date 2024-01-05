5dc7f6c18904bd1d086ebc875e848c4bf6e406fe
venkat <venkat.k@upstreamgs.com>
2024-01-05 13:42:47 +0530

    update

--- main.py
+++ main.py
@@ -8,10 +8,8 @@ def home():
 
 @app.route('/items/<int:item_id>', methods=['GET'])
 def get_items(item_id):
-    res = {"itemId":item_id, "description":"This is the item description"}
+    res = {"itemId":item_id, "description":"This is the item description "}
     return res
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
