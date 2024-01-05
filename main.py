d7800b3b4a011c7558a47165b72bf34267f96119
venkat <venkat.k@upstreamgs.com>
2024-01-05 13:29:16 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Welcome Home"
+
+@app.route('/items/<int:item_id>', methods=['GET'])
+def get_items(item_id):
+    res = {"itemId":item_id, "description":"This is the item description"}
+    return res
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
